8038c76946c3954be211ed709e63c74a9675b139
thembones79 <michal.szumnarski@gmail.com>
2025-06-08 19:42:36 +0200

    removed atoms

--- src/atoms/index.ts
+++
@@ -1,5 +0,0 @@
-import { atom, useAtom } from "jotai";
-// import { useAtom } from "jotai/ts3.8/react";
-
-export const formValues = atom<Record<string, string>>({});
-export const [form, setForm] = useAtom(formValues);
