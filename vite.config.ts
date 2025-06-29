eeb2ffeb066a90a61e4b3fd1fd0f86cdb2133366
thembones79 <michal.szumnarski@gmail.com>
2025-06-29 09:53:14 +0200

    005 - updated build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-${process.env.npm_package_version}.js`,
+        chunkFileNames: `assets/[name]-${process.env.npm_package_version}.js`,
+        assetFileNames: `assets/[name]-${process.env.npm_package_version}.[ext]`,
+      },
+    },
+  },
+});
