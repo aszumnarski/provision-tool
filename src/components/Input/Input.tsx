602d0680f1216eb69365c18a2ff3806e59efbc2e
thembones79 <michal.szumnarski@gmail.com>
2025-08-31 07:58:11 +0200

    now it is fixed

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,30 +2,31 @@ import "./Input.css";
 import { type IField } from "../Field/Field";
 
 export const Input = (props: IField) => {
-    const className = `field ${props.error ? "field--error" : ""}`;
-    const counter = (props.maxlength && props.value?.length)
-        ? `(Characters left: ${Number(props.maxlength) - props.value?.length})`
-        : "";
+  const className = `field ${props.error ? "field--error" : ""}`;
+  const counter =
+    props.maxlength && props.value?.length
+      ? `(Characters left: ${Number(props.maxlength) - props.value?.length})`
+      : "";
 
-    return (
-        <div className={className}>
-            <label>
-                <span>
-                    {props.label} {counter}
-                </span>
-                <input
-                    type={props.type}
-                    className="field-input"
-                    accept=".png,application/pdf"
-                    name={props.name}
-                    onChange={props.onChange}
-                    disabled={props.disabled}
-                    onBlur={props.onBlur}
-                    maxLength={Number(props.maxlength) || undefined}
-                    value={props.value || ""}
-                />
-            </label>
-            <p className="error-message">{props.error}</p>
-        </div>
-    );
+  return (
+    <div className={className}>
+      <label>
+        <span>
+          {props.label} {counter}
+        </span>
+        <input
+          type={props.type}
+          className="field-input"
+          accept=".png,application/pdf"
+          name={props.name}
+          onChange={props.onChange}
+          disabled={props.disabled}
+          onBlur={props.onBlur}
+          maxLength={Number(props.maxlength) || undefined}
+          value={props.value || ""}
+        />
+      </label>
+      <p className="error-message">{props.error}</p>
+    </div>
+  );
 };
